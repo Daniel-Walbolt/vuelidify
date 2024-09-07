c7ffa72ce5ed85c42e12016b918abc39263319f3
Daniel Walbolt <dwalbolt@gmail.com>
2024-09-06 22:37:15 -0400

    Refactor invocation process to remove duplicate validator result processing code.

--- src/services/validatorInvocation.ts
+++ src/services/validatorInvocation.ts
@@ -4,37 +4,110 @@ import { throttleQueueAsync } from "../finalFormUtilities";
 import { ProcessedValidator, PropertyValidationConfig } from "../privateTypes";
 import { processValidators } from "./validatorProcessing";
 
-type ThenCallback<G, KParent, Args, FValidationReturn> = (
+type ResultProcessor<G, KParent, Args, FValidationReturn> = (
 	processedValidator: ProcessedValidator<G, KParent, Args, FValidationReturn>,
 	ret: BaseValidationReturn<any>
-) => any;
+) => void;
 
 /** 
- * Handles invoking and optimization of the provided list of validators. 
+ * Handles invoking and optimization of the provided list of validators.
+ * @returns boolean for if all validators were valid
  */
-export function invokeAndOptimizeValidators<
+export async function invokeAndOptimizeValidators<
 	G, 
 	KParent,
 	Args,
 	FValidationReturn
 >(
-	property: G,
+	propertyConfig: PropertyValidationConfig<G, KParent, Args, FValidationReturn>,
 	parent: KParent | null | undefined,
 	args: Args,
-	latestProcessedValidators: ProcessedValidator<G, KParent, Args, FValidationReturn>[] = [],
-	/** The callback function for handling resolved validation promises */
-	thenCallback: ThenCallback<G, KParent, Args, FValidationReturn>,
-	shouldOptimize: boolean = true,
-	recursionCount: number = 1
+	validators: ProcessedValidator<G, KParent, Args, FValidationReturn>[],
+	/** Must match latest iteration ID on property config before updating any state. */
+	iterationId: number
 ) {
+	let isAllValid = true;
+	
+	// Create a callback to process the result of each validator.
+	const resultProcessor: ResultProcessor<G, KParent, Args, FValidationReturn> = (
+		processedValidator: ProcessedValidator<G, KParent, Args, FValidationReturn>,
+		ret: BaseValidationReturn<any>
+	) => {
+		// Don't perform any updates if this isn't the latest iteration
+		if (iterationId !== propertyConfig.validationIterationId) {
+			return;
+		}
+		if (ret.isValid === false) {
+			isAllValid = false;
+		}
+		// Identify reactive and lazy validators separately because they are run concurrently.
+		ret.identifier = processedValidator.validatorId;
+
+		// Check if this validation result already exists.
+		// Replace it if it does, otherwise add it.
+		const result = propertyConfig.reactiveValidationResults.value.find(x => x.identifier == ret.identifier);
+		if (result !== undefined) {
+			Object.assign(result, ret); 
+		}
+		else {
+			propertyConfig.reactiveValidationResults.value.push(ret);
+		}
+	}
+	const { asyncPromises, validatorsWhichPreviouslyReturnedValidators} = recursiveInvokeAndOptimizeValidators(
+		propertyConfig,
+		parent,
+		args,
+		validators,
+		iterationId,
+		resultProcessor,
+		true,
+		1
+	);
+	await Promise.all(asyncPromises);
+
+	if (iterationId === propertyConfig.validationIterationId) {
+		// Remove the error messages of validators that were ran in the previous run but not in this run.
+		for (const processedValidator of validatorsWhichPreviouslyReturnedValidators) {
+			for (const validatorId of Object.keys(processedValidator.previouslySpawnedValidators)) {
+				if (processedValidator.spawnedValidators[validatorId] == undefined) {
+					const index = propertyConfig.reactiveValidationResults.value.findIndex(x => x.identifier === validatorId);
+					if (index !== -1) {
+						propertyConfig.reactiveValidationResults.value.splice(index);
+					}
+				}
+			}
+		}
+	}
+
+	return isAllValid;
+}
+
+/**
+ * Private function that has extra parameterss for recursive calls and returns the promises for the validator results.
+ */
+function recursiveInvokeAndOptimizeValidators<
+	G, 
+	KParent,
+	Args,
+	FValidationReturn
+>(
+	propertyConfig: PropertyValidationConfig<G, KParent, Args, FValidationReturn>,
+	parent: KParent | null | undefined,
+	args: Args,
+	validators: ProcessedValidator<G, KParent, Args, FValidationReturn>[],
+	iterationId: number,
+	processValidatorResult: ResultProcessor<G, KParent, Args, FValidationReturn>,
+	shouldOptimize: boolean,
+	recursionCount: number
+) {
+	const property = propertyConfig.property.value;
 	// Collect all the promised results and synchronous results in lists to return later.
 	const allPromises: Promise<BaseValidationReturn>[] = [];
 	const allResults: BaseValidationReturn<any>[] = [];
 	// Add validators to this list that returned validators from a previous run, but did not this time.
 	const validatorsWhichPreviouslyReturnedValidators: ProcessedValidator<G, KParent, Args, FValidationReturn>[] = [];
-
-	for (const processedValidator of latestProcessedValidators) {
-		const shouldOptimizeValidator = !processedValidator.optimized;
+	for (const processedValidator of validators) {
+		const isValidatorAlreadyOptimized = processedValidator.optimized;
 		let checkForValidatorReturn = false;
 		if (processedValidator.previouslyReturnedValidators) {
 			processedValidator.previouslySpawnedValidators = processedValidator.spawnedValidators;
@@ -55,10 +128,11 @@ export function invokeAndOptimizeValidators<
 					}
 					if (Array.isArray(ret)) {
 						const { asyncPromises, syncResults } = handleReturnedValidators(
-							property,
+							propertyConfig,
 							parent,
 							args,
-							thenCallback,
+							iterationId,
+							processValidatorResult,
 							processedValidator,
 							ret,
 							recursionCount
@@ -70,10 +144,10 @@ export function invokeAndOptimizeValidators<
 					else if (checkForValidatorReturn) {
 						validatorsWhichPreviouslyReturnedValidators.push(processedValidator);
 					}
-					thenCallback(processedValidator, ret);
+					processValidatorResult(processedValidator, ret);
 				})
 			);
-			if (shouldOptimize && shouldOptimizeValidator) {
+			if (shouldOptimize && isValidatorAlreadyOptimized === false) {
 				processedValidator.optimized = true;
 				// Optimize async requests by adding a throttle to them.
 				processedValidator.validator = throttleQueueAsync<
@@ -89,10 +163,11 @@ export function invokeAndOptimizeValidators<
 			// This is because the list of validators returned COULD be dynamic, although it's very unlikely.
 			// It's impossible to know which validator is a previously ran validator because there's no ID attached to the function.
 			const { asyncPromises, syncResults } = handleReturnedValidators(
-				property,
+				propertyConfig,
 				parent,
 				args,
-				thenCallback,
+				iterationId,
+				processValidatorResult,
 				processedValidator,
 				validationReturn,
 				recursionCount
@@ -107,16 +182,15 @@ export function invokeAndOptimizeValidators<
 			// This check was added to support returning an array of promises for the validateIf() validator.
 			if (validationReturn !== undefined) {
 				allResults.push(validationReturn);
-				thenCallback(processedValidator, validationReturn);
+				processValidatorResult(processedValidator, validationReturn);
 			}
 		}
 	}
 	return {
-		syncResults: allResults,
-		/** The promised results from the async validators */
 		asyncPromises: allPromises,
+		syncResults: allResults,
 		validatorsWhichPreviouslyReturnedValidators
-	};
+	}
 }
 
 /** Takes the array of validators returned from a validator and adds them to the normal validation process. */
@@ -126,29 +200,28 @@ function handleReturnedValidators<
 	Args,
 	FValidationReturn
 >(
-	property: G,
+	propertyConfig: PropertyValidationConfig<G, KParent, Args, FValidationReturn>,
 	parent: KParent | null | undefined,
 	args: Args,
-	/** The callback function for handling resolved validation promises */
-	thenCallback: ThenCallback<G, KParent, Args, FValidationReturn>,
+	/** Must match latest iteration ID on property config before updating any state. */
+	iterationId: number,
+	processValidatorResult: ResultProcessor<G, KParent, Args, FValidationReturn>,
 	parentProcessedValidator: ProcessedValidator<G, KParent, Args, FValidationReturn>,
 	ret: Validator<G, KParent, Args, FValidationReturn>[],
 	recursionCount: number
 ) {
-	// Process the returned validators
 	const processedRetValidators = processValidators(
 		ret,
 		parentProcessedValidator.isReactive,
 		parentProcessedValidator.validatorId
 	);
-	// Enable support for returning a list of validators to run after a validator was ran.
-	// This feature was added for the validateIf() validator to handle async validators asynchronously from the synchronous validators.
-	const { asyncPromises, syncResults } = invokeAndOptimizeValidators(
-		property,
+	const response = recursiveInvokeAndOptimizeValidators(
+		propertyConfig,
 		parent,
 		args,
 		processedRetValidators,
-		thenCallback,
+		iterationId,
+		processValidatorResult,
 		false,
 		++recursionCount
 	);
@@ -160,10 +233,40 @@ function handleReturnedValidators<
 	parentProcessedValidator.spawnedValidators = spawnedValidatorsMap;
 	parentProcessedValidator.previouslyReturnedValidators = true;
 
-	return {
-		asyncPromises,
-		syncResults
-	};
+	return response;
+}
+
+function processValidatorResult<
+	G, 
+	KParent,
+	Args,
+	FValidationReturn
+>(
+	propertyConfig: PropertyValidationConfig<G, KParent, Args, FValidationReturn>,
+	processedValidator: ProcessedValidator<G, KParent, Args, FValidationReturn>,
+	ret: BaseValidationReturn,
+	/** Must match latest iteration ID on property config before updating any state. */
+	iterationId: number,
+	/** Is this result from a reactive or lazy validator? */
+	isReactive: boolean
+) {
+	let temp: BaseValidationReturn | undefined;
+	// Don't perform any updates if this isn't the latest iteration
+	if (iterationId != propertyConfig.validationIterationId) {
+		return;
+	}
+
+	ret.identifier = `${isReactive ? "reactive" : "lazy"}-${processedValidator.validatorId}`;
+
+	// Check if this validation result already exists.
+	// Replace it if it does, otherwise add it.
+	temp = propertyConfig.reactiveValidationResults.value.find(x => x.identifier == ret.identifier);
+	if (temp != undefined) {
+		Object.assign(temp, ret); 
+	}
+	else {
+		propertyConfig.reactiveValidationResults.value.push(ret);
+	}
 }
 
 /** Invokes all reactive validators for a property and returns whether or not they all passed. */
@@ -176,76 +279,24 @@ export async function invokeReactivePropertyValidators<
 	propertyConfig: PropertyValidationConfig<G, KParent, Args, FValidationReturn>,
 	parent: KParent | null | undefined,
 	args: Args,
-	/** Gives this concurrent iteration an ID which must match current iteration ID before updating the state. */
+	/** Must match latest iteration ID on property config before updating any state. */
 	iterationId: number
 ) {
 	propertyConfig.validatingReactive.value = true;
-	
-	// Assume every validator returns true. If any return false, this property will be set to false.
-	let allValid = true;
 
 	// Get the specified reactive validators and run them.
 	const reactiveValidators = propertyConfig.reactiveProcessedValidators;
-
-	/** Process the result of a validator and add it to the validation results. */
-	function processValidators(
-		processedValidator: ProcessedValidator<G, KParent, Args, FValidationReturn>,
-		ret: BaseValidationReturn
-	) {
-		let temp: BaseValidationReturn | undefined;
-		// Don't perform any updates if this isn't the latest iteration
-		if (iterationId != propertyConfig.validationIterationId) {
-			return;
-		}
-		if (ret.isValid == false) {
-			allValid = false;
-		}
-		ret.identifier = `reactive-${processedValidator.validatorId}`;
-
-		// Check if this validation result already exists.
-		// Replace it if it does, otherwise add it.
-		temp = propertyConfig.reactiveValidationResults.value.find(x => x.identifier == ret.identifier);
-		if (temp != undefined) {
-			Object.assign(temp, ret); 
-		}
-		else {
-			propertyConfig.reactiveValidationResults.value.push(ret);
-		}
-	}
-
-	console.time("Reactive Validation");
-	const reactiveValidationResults = invokeAndOptimizeValidators(
-		propertyConfig.property.value,
+	const isAllValid = await invokeAndOptimizeValidators(
+		propertyConfig,
 		parent,
 		args,
 		reactiveValidators,
-		processValidators
+		iterationId
 	);
-	console.timeEnd("Reactive Validation");
-
-	// Wait for all the asynchronous validators to finish before returning.
-	await Promise.all(reactiveValidationResults.asyncPromises);
 
 	// Only update the validation config if this is the latest validation iteration
-	if (iterationId == propertyConfig.validationIterationId) {
-		// Now all the processed validators should have the most up-to-date information.
-		// Loop through the validators that had previously returned validators.
-		console.log(propertyConfig.reactiveValidationResults.value);
-		for (const processedValidator of reactiveValidationResults.validatorsWhichPreviouslyReturnedValidators) {
-			console.log(processedValidator);
-			for (const validatorId of Object.keys(processedValidator.previouslySpawnedValidators)) {
-				if (processedValidator.spawnedValidators[validatorId] == undefined) {
-					// Remove the error messages associated with the previously ran validator.
-					const identifier = `reactive-${validatorId}`;
-					const index = propertyConfig.reactiveValidationResults.value.findIndex(x => x.identifier === identifier);
-					if (index !== -1) {
-						propertyConfig.reactiveValidationResults.value.splice(index);
-					}
-				}
-			}
-		}
-
-		propertyConfig.reactiveIsValid.value = allValid;
+	if (iterationId === propertyConfig.validationIterationId) {
+		propertyConfig.reactiveIsValid.value = isAllValid;
 		propertyConfig.validatingReactive.value = false;
 	}
 
@@ -267,50 +318,19 @@ export async function invokeLazyPropertyValidators<
 ) {
 	propertyConfig.validatingLazy.value = true;
 
-	// Assume every validator returns true. If any return false, this property will be set to false.
-	let allValid = true;
-
 	// Get the specified reactive validators and run them.
 	const lazyValidators = propertyConfig.lazyProcessedValidators;
-
-	/** Process the results of several validators and add them to the validation results with unique identifiers. */
-	function processValidators(
-		processedValidator: ProcessedValidator<G, KParent, Args, FValidationReturn>,
-		ret: BaseValidationReturn
-	) {
-		let temp: BaseValidationReturn | undefined;
-
-		if (iterationId != propertyConfig.validationIterationId) {
-			return;
-		}
-
-		if (ret.isValid == false) {
-			allValid = false;
-		}
-		ret.identifier = `lazy-${processedValidator.validatorId}`;
-		temp = propertyConfig.lazyValidationResults.value.find(x => x.identifier == ret.identifier);
-		if (temp != undefined) {
-			Object.assign(temp, ret);
-		}
-		else {
-			propertyConfig.lazyValidationResults.value.push(ret);
-		}
-	}
-
-	const lazyValidationResults = invokeAndOptimizeValidators(
-		propertyConfig.property.value,
+	const isAllValid = await invokeAndOptimizeValidators(
+		propertyConfig,
 		parent,
 		args,
 		lazyValidators,
-		processValidators
+		iterationId
 	);
 
-	// Wait for all the asynchronous validators to finish before returning.
-	await Promise.all(lazyValidationResults.asyncPromises);
-
 	// Only update the validation config if this is the latest validation iteration
-	if (iterationId == propertyConfig.validationIterationId) {
-		propertyConfig.lazyIsValid.value = allValid;
+	if (iterationId === propertyConfig.validationIterationId) {
+		propertyConfig.lazyIsValid.value = isAllValid;
 		propertyConfig.validatingLazy.value = false;
 	}
 
@@ -337,16 +357,16 @@ export async function invokeValidatorConfigs<KParent, Args, FValidationReturn>(
 	const validatorPromises: Promise<boolean | undefined>[] = [];
 	for (const validationConfig of validationConfigs) {
 		const iterationId = ++validationConfig.validationIterationId;
-		if (reactive && validationConfig.validation.$reactive != undefined) {
+		if (reactive && validationConfig.validation.$reactive !== undefined) {
 			validatorPromises.push(invokeReactivePropertyValidators(validationConfig, parent.value, args, iterationId));
 		}
 		// Check if we should validate lazy validators
-		if (lazy && validationConfig.validation.$lazy != undefined) {
+		if (lazy && validationConfig.validation.$lazy !== undefined) {
 			validatorPromises.push(invokeLazyPropertyValidators(validationConfig, parent.value, args, iterationId));
 		}
 		
-		// Check if there are array elements to validate. Each element can have it's own lazy or reactive properties.
-		if (validationConfig.elementValidation != undefined) {
+		// Check if there are array elements to validate. Each element can have its own lazy or reactive properties.
+		if (validationConfig.elementValidation !== undefined) {
 			const elementValidationConfigs: PropertyValidationConfig<any, KParent, Args, FValidationReturn>[] = [];
 			for (const key in validationConfig.arrayConfigMap) {
 				elementValidationConfigs.push(...validationConfig.arrayConfigMap[key].validationConfigs);
@@ -354,5 +374,6 @@ export async function invokeValidatorConfigs<KParent, Args, FValidationReturn>(
 			validatorPromises.push(invokeValidatorConfigs(elementValidationConfigs, parent, args, reactive, lazy));
 		}
 	}
-	return Promise.all(validatorPromises).then(response => response.every(x => x == true));
+	// Return true if all promises returned true, otherwise false.
+	return Promise.all(validatorPromises).then(response => response.every(x => x === true));
 }
