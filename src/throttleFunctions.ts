d8ac9a00bdd6d7c84a3400527ecd4c91582b74ae
Daniel Walbolt <dwalbolt@gmail.com>
2024-11-20 00:08:17 -0500

    Add example to reduceUndefined

--- src/throttleFunctions.ts
+++ src/throttleFunctions.ts
@@ -87,6 +87,11 @@ export function throttleQueueAsync<F extends (...args: any) => any, K>(
  * Accepts an array and uses the provided getter to get any value from each index ignoring any undefined values.
  * 
  * The default getter returns each array element (a map without the possible undefined values).
+ * 
+ * ```ts
+ * const array = [{ id: 123, name: "Foo"}, { id: 456, name: undefined }]
+ * const validNames = reduceUndefined(array, v => v.name) // ["Foo"]
+ * ```
  * @param array the array to map values from
  * @param getter the callback for each element which can return any nested value from each element.
  * @returns an array of the getter's return value invoked with each source element, with undefined values omitted.
