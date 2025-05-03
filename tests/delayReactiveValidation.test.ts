abf6a91faf468c982e8ba760dd6fa9ab004a5839
Daniel Walbolt <dwalbolt@gmail.com>
2025-05-03 11:25:09 -0400

    Create test for delayReactiveValidation config option

---
+++ tests/delayReactiveValidation.test.ts
@@ -0,0 +1,34 @@
+import { ref } from "vue";
+import { useValidation } from "../src/useValidation.ts";
+import { minLength } from "../src/validators.ts";
+import { assert } from "node:console";
+import { pause } from "./main.ts";
+
+Deno.test("Test delayReactiveValidation configuration", async (test: Deno.TestContext) => {
+	// Almost every other test relies on delayReactiveValidation: false.
+	// So, we really only need to test it being true here.
+	await test.step("Test delayReactiveValidation: true", testDelayedReactiveValidation);
+});
+
+const testDelayedReactiveValidation = async (test: Deno.TestContext) => {
+	const model = ref<string | number>();
+	const v$ = useValidation({
+		model: model,
+		validation: {
+			$reactive: [
+				minLength(10)
+			]
+		},
+		delayReactiveValidation: true
+	});
+	
+	model.value = "ThisIsLongEnough";
+	await pause();
+	assert(v$.isValid === false, "isValid was true when it was expected to be false. Reactive validation should not have executed.");
+	assert(v$.state.$state.resultsArray.length === 0, "Results array of the state was not empty. Reactive validation should not have executed.");
+	await v$.validate();
+	assert(v$.state.$state.resultsArray.length > 0, "Results array had zero length after calling validate. Reactive valiidation should have executed.");
+	model.value = "This";
+	await pause();
+	assert(v$.isValid === false, "isValid was true when it was expected to be false. Reactive validation should have executed and failed.");
+};
