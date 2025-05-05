0ca34945ad0e0c6d77ae7d223dcf1ead952fc5fb
Daniel Walbolt <dwalbolt@gmail.com>
2025-05-04 21:50:44 -0400

    Rename array validation test

--- tests/arrayValidation.test.ts
+++ tests/arrayValidation.test.ts
@@ -1,3 +1,9 @@
+import { ref } from "vue";
+import { useValidation } from "../src/useValidation.ts";
+import { assert } from "https://deno.land/std@0.224.0/assert/mod.ts";
+import { pause } from "./main";
+import { minLength } from "../src/validators.ts";
+
 Deno.test("Test Array Validation", async (test: Deno.TestContext) => {
 	await test.step("Primitive Array Validation", testPrimitiveArrayValidation);
 	await test.step("Basic Object Array Validation", testObjectArrayValidation);
@@ -7,6 +13,31 @@ Deno.test("Test Array Validation", async (test: Deno.TestContext) => {
 });
 
 const testPrimitiveArrayValidation = async (test: Deno.TestContext) => {
+	const model = ref<string[]>([]);
+	const reactiveValidationCount = ref(0);
+	const v$ = useValidation<string[]>({
+		model: model,
+		validation: {
+			$each: {
+				$reactive: [
+					minLength(10),
+				]
+			},
+			$reactive: [
+				(params) => {
+					reactiveValidationCount.value++;
+					return {
+						isValid: Math.random() > 0.5,
+						message: "This is an error message"
+					};
+				}
+			]
+		},
+		delayReactiveValidation: false
+	});
+	model.value.push("Test");
+	await pause();
+	assert(reactiveValidationCount.value > 0, "Array validation did not happen reactively after adding an element.");
 
 };
 
