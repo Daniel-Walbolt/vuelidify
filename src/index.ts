b7da553fd14779b5ffd1c276789b6e9fa4ae9b71
Daniel Walbolt <dwalbolt@gmail.com>
2025-05-11 15:52:57 -0400

    Add validateIf validator

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export * from "./publicTypes.ts";
-export { bufferAsync, throttleAsync, throttleBufferAsync, trailingDebounceAsync } from "./throttleFunctions.ts";
+export * from "./throttleFunctions.ts";
 export * from "./validators.ts";
 export { useValidation } from "./useValidation.ts";
