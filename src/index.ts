e111dd31fd152ad0fc62c918216a513790066113
Daniel Walbolt <dwalbolt@gmail.com>
2025-05-11 15:34:12 -0400

    deno format

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export * from "./publicTypes.ts";
-export { bufferAsync, throttleBufferAsync, trailingDebounceAsync, throttleAsync } from "./throttleFunctions.ts";
+export { bufferAsync, throttleAsync, throttleBufferAsync, trailingDebounceAsync } from "./throttleFunctions.ts";
 export * from "./validators.ts";
 export { useValidation } from "./useValidation.ts";
