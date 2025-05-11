d5ae63199268729a0588a98fc34a210282fced39
Daniel Walbolt <dwalbolt@gmail.com>
2025-05-11 14:40:23 -0400

    Adjust comments and rename throttle functions

--- src/services/validatorInvocation.ts
+++ src/services/validatorInvocation.ts
@@ -1,5 +1,5 @@
 import { computed, type MaybeRefOrGetter, type Ref, toValue } from "vue";
-import { bufferAsync, throttleQueueAsync } from "../throttleFunctions.ts";
+import { bufferAsync, throttleBufferAsync } from "../throttleFunctions.ts";
 import type {
 	GenericSyncValidator,
 	GenericValidator,
@@ -193,7 +193,7 @@ function recursiveInvokeAndOptimizeValidators(
 						) {
 							// Moderately slow validators will receive a throttle.
 							// Calls will overlap, but it shouldn't overwhelm the server
-							processedValidator.validator = throttleQueueAsync(processedValidator.validator, ThrottleDurationMs);
+							processedValidator.validator = throttleBufferAsync(processedValidator.validator, ThrottleDurationMs);
 						} else {
 							// Slow validators will receive a buffer.
 							// Calls will never overlap
--- src/throttleFunctions.ts
+++ src/throttleFunctions.ts
@@ -1,16 +1,18 @@
 import { Ref, ref } from "vue";
 
 /**
- * Buffers a function such that only the latest invocation will be executed after the previous finishes.
- * Intermediate calls resolve to undefined.
+ * Buffers a function such that only one instance of the function executes at a time.
+ *
+ * Calls during execution return a promise to execute the function after current execution finishes.
+ * Only the latest buffered call will execute the function; the rest resolve to undefined.
+ *
+ * @param func The function to apply a buffer to.
  *
  * @example
  * const buffered = bufferAsync(someAsyncFn);
  * buffered('a'); // executes
  * buffered('b'); // queued but will resolve to undefined
  * buffered('c'); // queued and will executed after first completes
- *
- * @param func The function to apply a buffer to
  */
 export function bufferAsync<F extends (...args: any[]) => any>(
 	func: F,
@@ -39,28 +41,28 @@ export function bufferAsync<F extends (...args: any[]) => any>(
 }
 
 /**
- * Throttles a function such that it is only called once per delay period.
+ * Throttles and buffers a function such that it is only called once per delay period.
  *
  * Calls during the throttle return a promise to execute the function after the throttle.
- * Only the latest queued call will execute; the rest resolve to undefined.
+ * Only the latest buffered call will execute; the rest resolve to undefined.
  *
  * Useful when you want to avoid spamming a resource and using the latest parameters is important.
  *
+ * @param func - The function to throttle.
+ * @param delayMs - Time between invocations.
+ * @returns A throttled version of the function.
+ *
  * @example
  * async function saveInput(input: string) { ... }
- * const throttledSave = throttleQueueAsync(saveInput, 1000);
+ * const throttledSave = throttleBufferAsync(saveInput, 1000);
  *
  * throttledSave("a"); // Executes immediately
  * throttledSave("b"); // Queued but will resolve to undefined
  * throttledSave("c"); // Replaces "b", starts after 1s
  *
  * // Only "a" and "c" will be processed
- *
- * @param func - The function to throttle.
- * @param delayMs - Time between invocations.
- * @returns A throttled version of the function.
  */
-export function throttleQueueAsync<F extends (...args: any[]) => any>(
+export function throttleBufferAsync<F extends (...args: any[]) => any>(
 	func: F,
 	delayMs: number,
 ): (...params: Parameters<F>) => Promise<Awaited<ReturnType<F>> | undefined> {
@@ -86,26 +88,24 @@ export function throttleQueueAsync<F extends (...args: any[]) => any>(
 }
 
 /**
- * Adds debounce behavior to a function.
+ * Adds trailing debounce behavior to a function.
  *
  * Waits for the specified delay after the last call before executing.
- * All previous calls during the delay are ignored.
- *
- * Useful for handling events like search input or autosave.
- *
- * @example
- * async function fetchResults(query: string) { ... }
- * const debouncedFetch = debounceAsync(fetchResults, 500);
- *
- * debouncedFetch("a");
- * debouncedFetch("ab");
- * debouncedFetch("abc"); // Only this call will be executed after 500ms
+ * All previous calls during the delay are ignored. Guarantees the call will have the latest parameters.
  *
  * @param func - The async function to debounce.
  * @param delay - Delay in milliseconds after the last call before execution.
  * @returns A debounced version of the function.
+ *
+ * @example
+ * async function fetchResults(query: string) { ... }
+ * const debouncedFetch = trailingDebounceAsync(fetchResults, 500);
+ *
+ * debouncedFetch("a");
+ * debouncedFetch("ab");
+ * debouncedFetch("abc"); // Only this call will be executed after 500ms
  */
-export function debounceAsync<F extends (...args: any) => any>(
+export function trailingDebounceAsync<F extends (...args: any) => any>(
 	func: F,
 	delayMs: number,
 ): (...params: Parameters<F>) => Promise<Awaited<ReturnType<F>> | undefined> {
@@ -116,14 +116,11 @@ export function debounceAsync<F extends (...args: any) => any>(
 		new Promise<Awaited<ReturnType<F>> | undefined>(
 			(resolve) => {
 				const currentId = ++callId;
-				if (timeout) {
-					// This is the first attempt at preventing execution of previous calls
-					// While not strictly necessary for safety, clearing timeouts as we don't
-					// need them is a performance consideration.
-					clearTimeout(timeout);
-				}
-				timeout = setTimeout(async () => {
+				setTimeout(async () => {
 					// This is the second attempt at preventing execution of previous calls
+					// if debounce is called sufficiently fast (i.e. programmatically)
+					// some iterations will pass the first if check at the same time, but
+					// this check won't succeed.
 					if (currentId == callId) {
 						resolve(await func(...params));
 					} else {
@@ -135,31 +132,40 @@ export function debounceAsync<F extends (...args: any) => any>(
 }
 
 /**
- * Throttles an async function, ensuring it's only called once per delay period.
+ * Throttles a function, ensuring it's called once per delay period.
  *
- * Calls during the delay are ignored. The first call after the delay runs immediately.
+ * Calls during the delay are ignored, and the first call after the delay executes immediately.
  *
- * Useful for hard limiting repeated actions (e.g., form submissions or API calls).
+ * Useful for hard limiting actions (e.g. form submissions or API calls).
  *
- * @param func - The async function to throttle.
- * @param delayMs - Minimum time (in ms) between allowed calls.
+ * @param func - The function to throttle.
+ * @param delayMs - The minimum time (in milliseconds) between calls.
  * @returns A ref indicating throttle state and the throttled function.
+ *
+ * @example
+ * async function fetchResults(query: string) { ... }
+ * const { throttledFunc } = throttleAsync(fetchResults, 500);
+ *
+ * throttledFunc("a"); // executes immediately
+ * throttledFunc("ab"); // ignored
+ * // ... 500 ms later ...
+ * throttledFunc("abc"); // executes immediately
  */
 export function throttleAsync<F extends (...args: any) => any>(
 	func: F,
 	delayMs: number,
 ): {
 	isThrottled: Ref<boolean>;
-	throttledFunc: (...params: Parameters<F>) => Promise<Awaited<ReturnType<F>> | undefined>;
+	throttledFunc: (...params: Parameters<F>) => ReturnType<F> | undefined;
 } {
 	const isThrottled: Ref<boolean> = ref(false);
-	const throttledFunc = async (...params: Parameters<F>) => {
+	const throttledFunc = (...params: Parameters<F>) => {
 		if (isThrottled.value) {
 			return undefined;
 		}
 		isThrottled.value = true;
 		setTimeout(() => isThrottled.value = false, delayMs);
-		return await func(...params);
+		return func(...params);
 	};
 	return {
 		isThrottled,
