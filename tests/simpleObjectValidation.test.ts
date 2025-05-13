e4674c0f16ed244367121c63f5aab7640d5253ca
Daniel Walbolt <dwalbolt@gmail.com>
2025-05-12 23:39:36 -0400

    Re-enable commented out steps of simple object validation. validateIf test is now working more reliably.

--- tests/simpleObjectValidation.test.ts
+++ tests/simpleObjectValidation.test.ts
@@ -1,16 +1,17 @@
-import { assert } from "https://deno.land/std@0.224.0/assert/mod.ts";
+import { assert, assertEquals } from "https://deno.land/std@0.224.0/assert/mod.ts";
 import { type Ref, ref } from "vue";
 import { useValidation } from "../src/useValidation.ts";
 import { minLength, must, validateIf } from "../src/validators.ts";
 import { pause } from "./main.ts";
+import { SyncValidator, Validator } from "../src/publicTypes.ts";
 
 Deno.test("Test Simple Object Validation", async (test: Deno.TestContext) => {
-	// await test.step("Test Must Validator", testMustEqualValidator);
+	await test.step("Test Must Validator", testMustEqualValidator);
 	await test.step("Test ValidateIf Validator", testValidateIfValidator);
-	// await test.step(
-	// 	"Test nullable object validation",
-	// 	testNullableObjectValidation,
-	// );
+	await test.step(
+		"Test nullable object validation",
+		testNullableObjectValidation,
+	);
 });
 
 type SimpleObject = {
@@ -65,41 +66,40 @@ const testValidateIfValidator = async (test: Deno.TestContext) => {
 		age: 1000,
 		isPerson: false,
 	});
-	// let v$ = useValidation({
-	// 	model: model,
-	// 	validation: {
-	// 		name: {
-	// 			$reactive: [
-	// 				validateIf((params) => params.model.isPerson === true, [minLength(5)]),
-	// 			],
-	// 		},
-	// 	},
-	// 	delayReactiveValidation: false,
-	// });
-	// model.value.isPerson = true;
-	// model.value.name = "Test";
-	// await pause();
-	// assert(
-	// 	v$.isValid === false,
-	// 	"isValid was true when the name is too long (minLength validator should be activating)",
-	// );
-	// model.value.isPerson = false;
-	// await pause();
-	// assert(
-	// 	v$.isValid === true,
-	// 	"isValid was false when validateIf should now return undefined and therefore pass validation.",
-	// );
+	let v$ = useValidation({
+		model: model,
+		validation: {
+			name: {
+				$reactive: [
+					validateIf((params) => params.model.isPerson === true, [minLength(5)]),
+				],
+			},
+		},
+		delayReactiveValidation: false,
+	});
+	model.value.isPerson = true;
+	model.value.name = "Test";
+	await pause();
+	assert(
+		v$.isValid === false,
+		"isValid was true when the name is too long (minLength validator should be activating)",
+	);
+	model.value.isPerson = false;
+	await pause();
+	assert(
+		v$.isValid === true,
+		"isValid was false when validateIf should now return undefined and therefore pass validation.",
+	);
 
 	let asyncPredicateRan: boolean = false;
 	// Now test that we can use async predicate as well
-	let v$ = useValidation({
+	v$ = useValidation({
 		model: model,
 		validation: {
 			name: {
 				$reactive: [
 					validateIf(async (params) => {
 						await pause(50);
-						console.log("Ran validate If predicate");
 						asyncPredicateRan = true;
 						return params.model.isPerson === true;
 					}, [minLength(5)]),
@@ -117,12 +117,94 @@ const testValidateIfValidator = async (test: Deno.TestContext) => {
 	);
 	model.value.isPerson = false;
 	await v$.validate();
-	await pause(20);
-	console.log(v$.state.name?.$state?.resultsArray, v$.isValid);
+	await pause(100);
 	assert(
 		v$.isValid === true,
 		"isValid was false when validateIf should now return undefined and therefore pass validation.",
 	);
+
+	// Now test to make sure that Vuelidify can handle unpredictable returns
+	// Create a validator that is recursive and has randomness to what it returns.
+	// Mix async and sync validators in the returned validators.
+	let expectedErrorMessages: string[] = [];
+	let depth = 0;
+
+	const Error1 = "ThisIsAnError";
+	const Error2 = "ThisIsAnotherError";
+
+	const createRandomValidator = (): SyncValidator => {
+		const randomValidator: SyncValidator = () => {
+			depth++;
+
+			if (Math.random() > 0.1) {
+				return [
+					() => {
+						const message = Error1;
+						const isValid = Math.random() > 0.5;
+						if (!isValid) {
+							expectedErrorMessages.push(message);
+						}
+						return {
+							isValid,
+							message,
+						};
+					},
+					createRandomValidator(),
+				];
+			} else if (Math.random() > 0.2) {
+				const randomValidators: Validator[] = [
+					async () => {
+						await pause(Math.random() * 50 + 30);
+						const message = Error2;
+						const isValid = Math.random() > 0.5;
+						if (!isValid) {
+							expectedErrorMessages.push(message);
+						}
+						return {
+							isValid,
+							message,
+						};
+					},
+				];
+				if (Math.random() > 0.5) {
+					randomValidators.push(createRandomValidator());
+				}
+				return randomValidators;
+			}
+		};
+
+		return randomValidator;
+	};
+
+	v$ = useValidation({
+		model: model,
+		validation: {
+			name: {
+				$lazy: [
+					() => {
+						depth = 0;
+						expectedErrorMessages = [];
+						return [createRandomValidator()];
+					},
+				],
+			},
+		},
+	});
+
+	const countOccurrences = (array: string[], match: string) => array.filter((x) => x === match).length;
+
+	for (let i = 0; i < 10; i++) {
+		await v$.validate();
+
+		// Assert the count of each error message rather than the positional arrangement of errors
+		// because we're not testing if our CPU can do concurrency in a specific order.
+		// Rather, we want to make sure the expected amount of errors are returned over several iterations.
+		assertEquals(
+			countOccurrences(v$.state.name?.$state?.errorMessages ?? [], Error1),
+			countOccurrences(expectedErrorMessages, Error1),
+			"Randomized nested validators did not return the expected error messages.",
+		);
+	}
 };
 
 const testNullableObjectValidation = async (test: Deno.TestContext) => {
