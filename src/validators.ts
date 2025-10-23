4723d6937ed2556369918ce560353f6ec87306eb
Daniel Walbolt <dwalbolt@gmail.com>
2025-10-22 20:15:11 -0400

    Add notEmpty, exclusiveMinNumber, and exclusiveMaxNumber validators. Add the ability to override the error message of any built-in validator which returns errors.

--- src/validators.ts
+++ src/validators.ts
@@ -2,92 +2,162 @@ import type { AsyncValidator, SyncValidator, Validator, ValidatorParams } from "
 
 /**
  * Validates the object is not loosely undefined.
+ * This does allow for empty strings. If you need a non-empty value, use {@link notEmpty()}.
+ * @param message sets the error message returned.
  */
-export function required(): SyncValidator {
+export function required(message?: string): SyncValidator {
 	return (params: ValidatorParams) => {
 		return {
 			isValid: params.value != undefined,
-			message: "This field is required",
+			message: message ?? "This field is required",
 		};
 	};
 }
 
 /**
+ * Validates the object is not loosely undefined and is not an empty string.
+ * @param message sets the error message returned.
+ */
+export function notEmpty(message?: string): SyncValidator {
+	return (params: ValidatorParams) => {
+		return {
+			isValid: params.value != undefined && params.value !== "" && String(params.value).trim().length > 0,
+			message: message ?? "Must not be empty"
+		}
+	}
+}
+
+/**
  * Validates a string or number has a length >= to the provided length. Undefined and null are 0 length.
- * @param min the minimum length of the string or number
+ * @param min the minimum length of the string or number.
+ * @param message sets the error message returned.
  */
 export function minLength<T extends string | number | undefined | null>(
 	min: number,
+	message?: string
 ): SyncValidator<T> {
 	return (params: ValidatorParams<T>) => {
 		const val = String(params.value ?? "");
 		return {
 			isValid: val.length >= min,
-			message: `Too short (${val.length} / ${min})`,
+			message: message ?? `Too short (${val.length} / ${min})`,
 		};
 	};
 }
 
 /**
  * Validates a string or number's length. Undefined and null are 0 length.
- * @param max the maximum length of the string or number
+ * @param max the maximum length of the string or number.
+ * @param message sets the error message returned.
  */
 export function maxLength<T extends string | number | undefined | null>(
 	max: number,
+	message?: string
 ): SyncValidator<T> {
 	return (params: ValidatorParams<T>) => {
 		const val = String(params.value ?? "");
 		return {
 			isValid: val.length <= max,
-			message: `Too long (${val.length} / ${max})`,
+			message: message ?? `Too long (${val.length} / ${max})`,
 		};
 	};
 }
 
 /**
  * Validates a number is defined and is at least some value.
- * @param min the minimum number the value can be
+ * @param min the minimum number the value can be.
+ * @param message sets the error message returned.
  */
 export function minNumber<T extends number | undefined | null>(
 	min: number,
+	message?: string
 ): SyncValidator<T> {
 	return (params: ValidatorParams<T>) => ({
-		isValid: params.value != undefined && params.value >= min,
-		message: `The minimum value is ${min}`,
+		isValid:
+			params.value != undefined
+			&& params.value >= min
+			// Added this check because number inputs in HTML will go to blank strings!
+			&& String(params.value) !== "",
+		message: message ?? `The minimum value is ${min}`,
+	});
+}
+
+/**
+ * Validates a number is defined and is at least some value.
+ * @param min the minimum number the value can be.
+ * @param message sets the error message returned.
+ */
+export function exclusiveMinNumber<T extends number | undefined | null>(
+	min: number,
+	message?: string
+): SyncValidator<T> {
+	return (params: ValidatorParams<T>) => ({
+		isValid:
+			params.value != undefined
+			&& params.value > min
+			// Added this check because number inputs in HTML will go to blank strings!
+			&& String(params.value) !== "",
+		message: message ?? `The minimum value is ${min}`,
+	});
+}
+
+
+/**
+ * Validates a number is defined and is at most some value.
+ * @param max the maximum number the value can be.
+ * @param message sets the error message returned.
+ */
+export function maxNumber<T extends number | undefined | null>(
+	max: number,
+	message?: string
+): SyncValidator<T> {
+	return (params: ValidatorParams<T>) => ({
+		isValid:
+			params.value != undefined
+			&& params.value <= max
+			// Added this check because number inputs in HTML will go to blank strings!
+			&& String(params.value) !== "",
+		message: message ?? `The maximum value is ${max}`,
 	});
 }
 
 /**
  * Validates a number is defined and is at most some value.
- * @param max the maximum number the value can be
+ * @param max the maximum number the value can be.
+ * @param message sets the error message returned.
  */
-export function maxNumber<T extends number | undefined | null>(
+export function exclusiveMaxNumber<T extends number | undefined | null>(
 	max: number,
+	message?: string
 ): SyncValidator<T> {
 	return (params: ValidatorParams<T>) => ({
-		isValid: params.value != undefined && params.value <= max,
-		message: `The maximum value is ${max}`,
+		isValid:
+			params.value != undefined
+			&& params.value < max
+			// Added this check because number inputs in HTML will go to blank strings!
+			&& String(params.value) !== "",
+		message: message ?? `The maximum value is ${max}`,
 	});
 }
 
 /**
  * Validate the provided predicate function.
  * @param fn predicate that returns true if the value is valid.
- * @param errorMessage the message to display when the values are not equal.
+ * @param message sets the error message returned.
  */
 export function must<T, K, V, R, A>(
 	fn: (params: ValidatorParams<T, K, V, A>) => boolean,
-	errorMessage: string,
+	message: string,
 ): SyncValidator<T, K, V, R, A> {
 	return (params) => ({
 		isValid: fn(params),
-		message: errorMessage,
+		message: message,
 	});
 }
 
 /**
- * Execute a set of validators only if the provided predicate is true
- * @param predicate Determines if the set of validators should be returned
+ * Execute a set of validators only if the provided predicate is true.
+ * @param predicate determines if the set of validators should be returned.
  * @param validators The set of validators to execute if the predicate returns true.
  */
 export function validateIf<T, K, V, R, A, Validators extends Validator<T, K, V, R, A>[]>(
@@ -105,16 +175,19 @@ export function validateIf<T, K, V, R, A, Validators extends Validator<T, K, V, 
  * Validates a string is a valid looking email using RegEx.
  *
  * The RegEx was taken from https://stackoverflow.com/questions/46155/how-can-i-validate-an-email-address-in-javascript, and may be updated in the future.
-s */
+ * @param message sets the error message returned.
+ */
 export function isEmailSync<
 	T extends string | undefined | null,
->(): SyncValidator<T> {
+>(
+	message?: string
+): SyncValidator<T> {
 	return (params: ValidatorParams<T>) => ({
 		isValid: params.value
 			? RegExp(
 				/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
 			).test(params.value)
 			: false,
-		message: "Invalid email format",
+		message: message ?? "Invalid email format",
 	});
 }
