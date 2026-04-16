ab5fef547dbad3e9f104675f36fb9af9d0059362
Daniel Walbolt <dwalbolt@gmail.com>
2026-04-15 22:03:36 -0400

    fix: enhance nullable object validation to test properties on nullable objects.

--- tests/simpleObjectValidation.test.ts
+++ tests/simpleObjectValidation.test.ts
@@ -1,7 +1,7 @@
 import { assert, assertEquals } from "https://deno.land/std@0.224.0/assert/mod.ts";
 import { type Ref, ref } from "vue";
 import { useValidation } from "../src/useValidation.ts";
-import { minLength, must, validateIf } from "../src/validators.ts";
+import { minLength, must, required, validateIf } from "../src/validators.ts";
 import { pause } from "./main.ts";
 import type { SyncValidator, Validator } from "../src/publicTypes.ts";
 
@@ -208,40 +208,69 @@ const testValidateIfValidator = async (test: Deno.TestContext) => {
 };
 
 const testNullableObjectValidation = async (test: Deno.TestContext) => {
+	// The nullable object we're testing is the relatedEntity.
 	const model: Ref<SimpleObject> = ref({
 		age: 10,
 		name: "Foo",
+		relatedEntity: null,
 	});
 	const v$ = useValidation({
 		model: model,
 		validation: {
 			relatedEntity: {
 				$reactive: [
-					must((p) => p.value != undefined, "Related entity must be defined"),
+					required(),
 				],
+				confirmPassword: {
+					$reactive: [required()]
+				}
 			},
 		},
 		delayReactiveValidation: false,
 	});
-	await v$.validate();
+
+	// Make sure validation state is set even though the object being validated doesn't exist.
 	assert(
 		v$.state.relatedEntity?.$state != undefined,
-		"$state was not defined for a nested object that has validation rules defined for it",
+		"$state was not defined for a nullable object that has validation rules defined for it",
 	);
-	assert(
-		v$.state.relatedEntity?.$state?.isValid === false,
-		"Validation on undefined nested object passed when it should have failed.",
-	);
-	model.value.relatedEntity = null;
-	await pause();
+
+	// Now test null values correctly pass null into the validators and fail
+	// because the validators expect non-null.
+	await v$.validate();
 	assert(
 		v$.state.relatedEntity?.$state?.isValid === false,
 		"Validation on null nested object passed when it should have failed.",
+	);
+	assert(
+		v$.state.relatedEntity?.$state?.isValid === false,
+		"Validation on null property in a null object passed when it should have failed."
+	)
+
+	// Now test undefined values behave the same way.
+	model.value.relatedEntity = undefined;
+	await pause();
+	assert(
+		v$.state.relatedEntity?.$state?.isValid === false,
+		"Validation on undefined nested object passed when it should have failed.",
 	);
 	model.value.relatedEntity = {};
 	await pause();
 	assert(
 		v$.state.relatedEntity?.$state?.isValid === true,
 		"Validation on nested object failed when it should have passed",
+	);
+
+	assert(
+		v$.state.relatedEntity?.confirmPassword?.$state?.isValid === false,
+		"Validation on a newly created object passed when confirmPassword was still missing.",
+	);
+	model.value.relatedEntity.confirmPassword = "secret";
+	await pause();
+
+	// Test that validation correctly receives nested values that used to be null.
+	assert(
+		v$.state.relatedEntity?.confirmPassword?.$state?.isValid === true,
+		"confirmPassword validation on originally null object did not pass when it became set.",
 	);
 };
